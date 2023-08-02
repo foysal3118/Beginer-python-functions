569e3f9c2777d7846a3551a7f7f21b5951068dce
Faysal Durlov <125384302+foysal3118@users.noreply.github.com>
2023-08-02 10:52:25 -0700

    Update something.py

--- something.py
+++ something.py
@@ -17,7 +17,7 @@ def getfib(a,b,n):
       b = c
     return
     print(c)
-a= int(input())
-b = int(input())
-c = int(input())
-getfib(a,b,c)
+x = int(input())
+y = int(input())
+z = int(input())
+getfib(x,y,z)
